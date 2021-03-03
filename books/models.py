3b10ef0b7e72a45a271ebf0391ebaf2ebd225f18
kunxin-chor <chorkunxin@yahoo.com>
2021-03-03 12:43:17 +0000

    Added models and crud demo

--- books/models.py
+++ books/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Book(models.Model):
+    # define a VARCHAR using Django
+    title = models.CharField(blank=False, max_length=255)
+    ISBN = models.CharField(blank=False, max_length=255)
+
+    # TextField is the TEXT in MySQL (65000 characters)
+    desc = models.TextField(blank=False)
+
+    def __str__(self):
+        return self.title
--- books/views.py
+++ books/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from .models import Book
 
 # Create your views here.
 
 
 # define a view function
 def index(request):
-    return render(request, "books/index-template.html")
+    books = Book.objects.all()  # eqv. SELECT * FROM books
+    return render(request, "books/index-template.html", {
+        'books': books
+    })
